eac0d4c87ad405d6d625ae49b50f00f07ba76d47
Matteo Bruni <ar3s@icloud.com>
2020-08-01 00:55:21 +0200

    Updates

---
+++ precache-manifest.b1317ac99dff598db97d7bfec9877386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010588e1b662f3aed3e89578e650c2e8",
+    "url": "/react-landing-page-template/index.html"
+  },
+  {
+    "revision": "9ca0107d6ace4237f45c",
+    "url": "/react-landing-page-template/static/css/main.0175611e.chunk.css"
+  },
+  {
+    "revision": "384a70945d4426b2e804",
+    "url": "/react-landing-page-template/static/js/2.e732e586.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/react-landing-page-template/static/js/2.e732e586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca0107d6ace4237f45c",
+    "url": "/react-landing-page-template/static/js/main.dcbc0a7f.chunk.js"
+  },
+  {
+    "revision": "3ef94428c60e23d1637e",
+    "url": "/react-landing-page-template/static/js/runtime-main.d41e6e1c.js"
+  }
+]);
